859123a228420c3c8598eca076cc4f65d4ee76da
Krzysztof Sikora <sikora@humanaction.eu>
2018-05-15 11:04:20 +0200

    Added production build options

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,13 @@ module.exports = {
                name: 'vendor'
              }),
         new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+		new webpack.DefinePlugin({
+			'process.env': {
+			  'NODE_ENV': JSON.stringify('production')
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin()
     ],
     devtool: 'inline-source-map',
     devServer: {
