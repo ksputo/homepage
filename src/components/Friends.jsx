0f56cedb014e2168882cbb2a801d5506eb2b5937
Krzysztof Sikora <sikora@humanaction.eu>
2018-09-16 10:38:05 +0200

    Added new sponsors and meetups for the landing page

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -3,12 +3,18 @@ import React, { Component } from "react";
 import meetjs from '../images/meetjs-logo-white-katowice.svg'
 import microsoft from '../images/microsoft.png'
 import azure from '../images/azure.png'
+import qa from '../images/quality-meetup.png'
 import Logos from './Logos';
 
 class Friends extends Component {
   render() {
     return (
-      <Logos title='Kamraci' logos={[{ image: meetjs, url: 'https://katowice.meetjs.pl/' }, { image: microsoft }, {image: azure }]} className='friends' backgroundColor='grey'/>
+      <Logos title='Kamraci' logos={[
+        { image: meetjs, url: 'https://katowice.meetjs.pl/' },
+        { image: microsoft },
+        { image: azure },
+        { image: qa }
+      ]} className='friends' backgroundColor='grey'/>
     );
   }
 }
--- src/components/Supporters.jsx
+++ src/components/Supporters.jsx
@@ -2,10 +2,17 @@ import React, { Component } from "react";
 import Logos from './Logos'
 import futureProcessingLogo from "../images/future-processing__logo-2017-white.png";
 import katowiceLogo from "../images/katowice_logo.png";
+import softserve from '../images/soft-serve.svg';
+import isystems from '../images/i-systems-logo.png';
 
 class Supporters extends Component {
   render() {
-    return <Logos logos={[ { image: futureProcessingLogo }, { image: katowiceLogo }]} title='Wspierają nas' backgroundColor='white'/>;
+    return <Logos logos={[
+      { image: futureProcessingLogo },
+      { image: katowiceLogo },
+      { image: softserve },
+      { image: isystems }
+    ]} title='Wspierają nas' backgroundColor='white'/>;
   }
 }
 
