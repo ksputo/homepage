3e80e308c379faa0f52d780e5d76e607c2b8f37d
Krzysztof Sikora <sikora@humanaction.eu>
2018-10-22 16:09:00 +0200

    Added brainhub logo

--- src/components/Supporters.jsx
+++ src/components/Supporters.jsx
@@ -5,15 +5,17 @@ import katowiceLogo from "../images/katowice_logo.png";
 import softserve from '../images/soft-serve.svg';
 import isystems from '../images/i-systems-logo.png';
 import billennium from '../images/logo_billennium-01.png';
+import brainhub from '../images/brainhub.svg';
 
 class Supporters extends Component {
   render() {
     return <Logos logos={[
-      { image: futureProcessingLogo },
-      { image: katowiceLogo },
-      { image: softserve },
-      { image: isystems },
-      { image: billennium }
+      { image: futureProcessingLogo, url: 'https://future-processing.pl/' },
+      { image: katowiceLogo, url: 'http://miasto-ogrodow.eu/' },
+      { image: softserve, url: 'https://softserveinc.com/en-us/' },
+      { image: isystems, url: 'https://www.i-systems.pl/' },
+      { image: billennium, url: 'https://billennium.pl/' },
+      { image: brainhub, url: 'https://brainhub.eu/' }
     ]} title='Wspierają nas' backgroundColor='white'/>;
   }
 }
