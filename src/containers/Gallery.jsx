37cc743cd235190171f4e405e21befa8def761f1
SebastianWolski <sebastian.wolski96@gmail.com>
2018-10-27 19:03:00 +0200

    Removed unused code

--- src/containers/Gallery.jsx
+++ src/containers/Gallery.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "grommet/components/Header";
-import Menu from "grommet/components/Menu";
-import Anchor from "grommet/components/Anchor";
 import Grommet from "grommet/components/Grommet";
 
 import Footer from '../components/Footer';
